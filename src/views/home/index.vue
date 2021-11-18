a052f8e1f512f52d594b6326979acd5019698060
小生 <1569775675@qq.com>
2021-11-18 16:32:56 +0800

    some setting

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,62 +8,65 @@
 </template>
 
 <script>
-
-
-
-import {gotoTarget} from '@/api/map/map.js'
+import { gotoTarget } from '@/api/map/map.js'
 export default {
   data() {
     return {
-      gaodeUrl:'',
-      baiduUrl:''
+      gaodeUrl: '',
+      baiduUrl: ''
     }
   },
 
   computed: {},
   created() {
-    gotoTarget({address:'北京市朝阳区阜通东大街6号'}).then(gaodeObj=>{
-      console.log(gaodeObj);
+    gotoTarget({ address: '北京市朝阳区阜通东大街6号' }).then(gaodeObj => {
+      console.log(gaodeObj)
       this.gaodeUrl = gaodeObj.gaodeUrl
       this.gaodeObj = gaodeObj.to
     })
   },
   methods: {
-  openMap(e){
-     const u = navigator.userAgent;
-    const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-    console.log(`https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`);
-  if (isAndroid) {
-    //这个是安卓操作系统
-				var d = new Date();
-   				var t0 = d.getTime();
-    window.location.href = this.gaodeUrl
-		//判断是否切出浏览器
-    setTimeout(()=>{
-		let hidden = window.document.hidden || window.document.mozHidden || window.document.msHidden ||window.document.webkitHidden 
-		if(typeof hidden =="undefined" || hidden ==false){
-		//应用宝下载地址
-		window.location.href =`https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`;
-		}
-		}, 2500);
-  }else {
-			    //这个是ios操作系统
-				var d = new Date();
-   				var t0 = d.getTime();
-				  window.location.href = this.gaodeUrl
-				//判断是否切出浏览器
-	  			 setTimeout(()=>{
-		                let hidden = window.document.hidden || window.document.mozHidden || window.document.msHidden ||window.document.webkitHidden 
-		                if(typeof hidden =="undefined" || hidden ==false){
-		                	//应用宝下载地址
-		                window.location.href =`https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`;
-		                }
-		            }, 2500);
-			}
+    openMap(e) {
+      const u = navigator.userAgent
+      const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1 //android终端
+      console.log(`https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`)
+      if (isAndroid) {
+        //这个是安卓操作系统
+        var d = new Date()
+        var t0 = d.getTime()
+        window.location.href = this.gaodeUrl
+        //判断是否切出浏览器
+        setTimeout(() => {
+          let hidden =
+            window.document.hidden ||
+            window.document.mozHidden ||
+            window.document.msHidden ||
+            window.document.webkitHidden
+          if (typeof hidden == 'undefined' || hidden == false) {
+            //应用宝下载地址
+            window.location.href = `https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`
+          }
+        }, 2500)
+      } else {
+        //这个是ios操作系统
+        var d = new Date()
+        var t0 = d.getTime()
+        window.location.href = this.gaodeUrl
+        //判断是否切出浏览器
+        setTimeout(() => {
+          let hidden =
+            window.document.hidden ||
+            window.document.mozHidden ||
+            window.document.msHidden ||
+            window.document.webkitHidden
+          if (typeof hidden == 'undefined' || hidden == false) {
+            //应用宝下载地址
+            window.location.href = `https://uri.amap.com/marker?position=${this.gaodeObj.longitude},${this.gaodeObj.latitude}`
+          }
+        }, 2500)
+      }
     }
   }
 }
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
